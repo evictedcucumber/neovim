a4614efce99e3e649902d618a2f6fa409e49a096
ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-03-07 20:27:21 +0200

    feat: replaced autopairs with mini-autopairs

--- lua/ethan/plugins/config/autopairs.lua
+++
@@ -1,6 +0,0 @@
-return function()
-    require('nvim-autopairs').setup()
-
-    local cmp = require('cmp')
-    cmp.event:on('confirm_done', require('nvim-autopairs.completion.cmp').on_confirm_done)
-end
