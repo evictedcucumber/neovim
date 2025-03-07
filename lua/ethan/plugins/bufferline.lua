4e29b825e090d130a63818b6b68086f0ef22c138
ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-03-07 20:27:07 +0200

    feat: added bufferline

---
+++ lua/ethan/plugins/bufferline.lua
@@ -0,0 +1,21 @@
+return {
+    'akinsho/bufferline.nvim',
+    tag = 'v4.6.1',
+    after = 'catppuccin',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+        require('bufferline').setup({
+            highlights = require('catppuccin.groups.integrations.bufferline').get(),
+            options = {
+                diagnostics = 'nvim_lsp',
+                show_close_icon = false,
+                show_buffer_close_icons = false,
+                always_show_bufferline = false,
+                auto_toggle_bufferline = true,
+                hover = {
+                    enabled = false,
+                },
+            },
+        })
+    end,
+}
