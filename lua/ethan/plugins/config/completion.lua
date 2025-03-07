5ac738f6e252cd0e9c72c504d8df7c5ce4fd52b4
ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-03-07 20:27:27 +0200

    feat: improve completion ui and functionality

--- lua/ethan/plugins/config/completion.lua
+++ lua/ethan/plugins/config/completion.lua
@@ -7,30 +7,63 @@ return function()
 
     cmp.setup({
         completion = {
-            completeopt = 'menu,menuone,preview,noselect',
+            completeopt = 'menu,menuone,noselect',
+        },
+        window = {
+            completion = cmp.config.window.bordered({
+                scrollbar = false,
+            }),
+            documentation = cmp.config.window.bordered({
+                side_padding = 0,
+            }),
         },
         snippet = {
             expand = function(args)
                 luasnip.lsp_expand(args.body)
             end,
         },
-        mapping = cmp.mapping.preset.insert({
+        mapping = {
             ['<C-k>'] = cmp.mapping.select_prev_item(),
             ['<C-j>'] = cmp.mapping.select_next_item(),
             ['<C-b>'] = cmp.mapping.scroll_docs(-4),
             ['<C-f>'] = cmp.mapping.scroll_docs(4),
             ['<C-Space>'] = cmp.mapping.complete(),
             ['<C-e>'] = cmp.mapping.abort(),
-            ['<CR>'] = cmp.mapping.confirm({ select = false }),
-        }),
+            ['<CR>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    if luasnip.expandable() then
+                        luasnip.expand()
+                    else
+                        cmp.confirm({ select = false })
+                    end
+                else
+                    fallback()
+                end
+            end),
+            ['<Tab>'] = cmp.mapping(function(fallback)
+                if luasnip.locally_jumpable(1) then
+                    luasnip.jump(1)
+                else
+                    fallback()
+                end
+            end, { 'i', 's' }),
+            ['<S-Tab>'] = cmp.mapping(function(fallback)
+                if luasnip.locally_jumpable(-1) then
+                    luasnip.jump(-1)
+                else
+                    fallback()
+                end
+            end, { 'i', 's' }),
+        },
         sources = cmp.config.sources({
             { name = 'nvim_lsp' },
-            { name = 'buffer' },
             { name = 'luasnip' },
+        }, {
+            { name = 'buffer' },
             { name = 'path' },
         }),
-        ---@diagnostic disable-next-line: missing-fields
         formatting = {
+            fields = { 'abbr', 'kind', 'menu' },
             format = lspkind.cmp_format({
                 maxwidth = 50,
                 ellipsis_char = '...',
@@ -38,15 +71,22 @@ return function()
         },
     })
 
+    local cmdline_mapping = {
+        ['<C-k>'] = { c = cmp.mapping.select_prev_item() },
+        ['<C-j>'] = { c = cmp.mapping.select_next_item() },
+        ['<C-e>'] = { c = cmp.mapping.abort() },
+        ['<CR>'] = { c = cmp.mapping.confirm({ select = false }) },
+    }
+
     cmp.setup.cmdline('/', {
-        mapping = cmp.mapping.preset.cmdline(),
+        mapping = cmdline_mapping,
         sources = {
             { name = 'buffer' },
         },
     })
 
     cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
+        mapping = cmdline_mapping,
         sources = cmp.config.sources({
             { name = 'cmdline', option = { ignore_cmds = { 'Man', '!' } } },
             { name = 'path' },
