592ac595e26466587430cccf9b6798c7b7b093dd
ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-03-07 20:27:59 +0200

    chore: format

--- lua/ethan/plugins/noice.lua
+++ lua/ethan/plugins/noice.lua
@@ -7,42 +7,30 @@ return {
     event = 'VeryLazy',
     opts = {
         cmdline = {
-            enabled = true,
             view = 'cmdline',
+            format = { search_down = { view = 'cmdline' }, search_up = { view = 'cmdline' } },
         },
         routes = {
             {
                 filter = { event = 'notify', find = 'No information available' },
                 opts = { skip = true },
             },
+            {
+                filter = { event = 'msg_show', kind = '', find = 'written' },
+                opts = { skip = true },
+            },
         },
-        notify = {
-            enabled = true,
-            view = 'mini',
-        },
+        notify = { enabled = true, view = 'mini' },
         lsp = {
-            progress = {
-                enabled = false,
-            },
-            hover = {
-                enabled = true,
-                silent = true,
-            },
-            message = {
-                enabled = true,
-                view = 'mini',
-            },
+            progress = { enabled = false },
+            hover = { enabled = true, silent = true },
+            message = { enabled = true, view = 'mini' },
             override = {
                 ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
                 ['vim.lsp.util.stylize_markdown'] = true,
                 ['cmp.entry.get_documentation'] = true,
             },
         },
-        messages = {
-            enabled = true,
-            view = 'mini',
-            view_error = 'mini',
-            view_warn = 'mini',
-        },
+        messages = { enabled = true, view = 'mini', view_error = 'mini', view_warn = 'mini' },
     },
 }
