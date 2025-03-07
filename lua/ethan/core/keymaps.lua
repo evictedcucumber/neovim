947d10334f5fdbab8bc326bed42608ff5ce5e93e
ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-03-07 20:26:26 +0200

    feat: added easy escape keybind when in insert mode

--- lua/ethan/core/keymaps.lua
+++ lua/ethan/core/keymaps.lua
@@ -5,6 +5,8 @@ vim.keymap.set('n', '<leader><tab>', '<cmd>Oil<CR>', { desc = 'Open Oil' })
 
 vim.keymap.set('n', '<ESC>', '<cmd>nohlsearch<CR>')
 
+vim.keymap.set('i', 'jj', '<ESC>')
+
 vim.keymap.set('n', '<leader>wsv', '<C-w>v', { desc = '[W]indow [S]plit [V]ertial' })
 vim.keymap.set('n', '<leader>wsh', '<C-w>s', { desc = '[W]indow [S]plit [H]orizontal' })
 vim.keymap.set('n', '<leader>wsx', '<cmd>close<CR>', { desc = '[W]indow [S]plit Close' })
