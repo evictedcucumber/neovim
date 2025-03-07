f363cc7f160ee207a56e61eded63cc791ef35481
ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-03-07 20:26:37 +0200

    feat: added colour column at 100

--- lua/ethan/core/options.lua
+++ lua/ethan/core/options.lua
@@ -3,6 +3,8 @@ vim.g.have_nerd_font = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+vim.opt.colorcolumn = '100'
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
