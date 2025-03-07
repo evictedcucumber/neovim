055fa0281258c3e61532d772409fee27d6afa465
ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-03-07 20:27:44 +0200

    feat(deps): Added mini-icons

--- lua/ethan/plugins/oil.lua
+++ lua/ethan/plugins/oil.lua
@@ -1,6 +1,6 @@
 return {
     'stevearc/oil.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = { 'nvim-tree/nvim-web-devicons', 'echasnovski/mini.nvim' },
     event = 'VimEnter',
     config = function()
         require('oil').setup({
