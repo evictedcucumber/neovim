1731ab39c23bae6ec3420d8b5067e5d780ccbe8a
ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-03-07 20:27:47 +0200

    feat(config): Added copilot init

--- lua/ethan/plugins/copilot.lua
+++ lua/ethan/plugins/copilot.lua
@@ -1,4 +1,14 @@
 return {
     'github/copilot.vim',
     event = { 'BufReadPre', 'BufNewFile' },
+    init = function()
+        vim.g.copilot_no_tab_map = true
+        vim.keymap.set(
+            'i',
+            '<M-c>',
+            'copilot#Accept("\\<CR>")',
+            { expr = true, replace_keycodes = false }
+        )
+        vim.keymap.set('i', '<C-e>', '<Plug>(copilot-dismiss)')
+    end,
 }
