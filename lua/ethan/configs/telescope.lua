0b5f8cb67e19b44b2321bb61c7f39c85ee39db71
ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-03-07 20:27:09 +0200

    feat: fixed issue with new deprecation of trouble methods

--- lua/ethan/configs/telescope.lua
+++ lua/ethan/configs/telescope.lua
@@ -8,7 +8,7 @@ M.setup = function()
     local transform_mod = require('telescope.actions.mt').transform_mod
 
     local trouble = require('trouble')
-    local trouble_telescope = require('trouble.providers.telescope')
+    local trouble_telescope = require('trouble.sources.telescope')
 
     local custom_actions = transform_mod({
         open_trouble_qflist = function()
@@ -28,7 +28,7 @@ M.setup = function()
                     ['<C-j>'] = actions.move_selection_next,
                     ['<C-q>'] = actions.send_selected_to_qflist
                         + custom_actions.open_trouble_qflist,
-                    ['<C-t>'] = trouble_telescope.smart_open_with_trouble,
+                    ['<C-t>'] = trouble_telescope.open,
                 },
             },
         },
