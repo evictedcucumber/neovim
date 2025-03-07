eac970187d9a3990269b44e949a33aff8b494519
ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-03-07 20:27:35 +0200

    fix: fixed clash with flash and surround

--- lua/ethan/plugins/flash.lua
+++ lua/ethan/plugins/flash.lua
@@ -2,10 +2,30 @@ return {
     'folke/flash.nvim',
     event = 'VeryLazy',
     opts = {},
-    -- stylua: ignore
     keys = {
-        { 's', function() require('flash').jump() end, mode = { 'n', 'o', 'x' }, desc = 'Flash Jump', },
-        { 'S', function() require('flash').treesitter() end, mode = { 'n', 'o', 'x' }, desc = 'Flash Treesitter', },
-        { '<C-s>', function() require('flash').toggle() end, mode = { 'c' }, desc = 'Flash Toggle', },
+        {
+            'm',
+            function()
+                require('flash').jump()
+            end,
+            mode = { 'n', 'o', 'x' },
+            desc = 'Flash Jump',
+        },
+        {
+            'M',
+            function()
+                require('flash').treesitter()
+            end,
+            mode = { 'n', 'o', 'x' },
+            desc = 'Flash Treesitter',
+        },
+        {
+            '<C-m>',
+            function()
+                require('flash').toggle()
+            end,
+            mode = { 'c' },
+            desc = 'Flash Toggle',
+        },
     },
 }
