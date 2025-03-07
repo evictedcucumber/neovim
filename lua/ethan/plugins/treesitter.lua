dde2c88a02dad5bd942018e57c467e16af5289cf
ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-03-07 20:27:18 +0200

    feat: fix treesitter

--- lua/ethan/plugins/treesitter.lua
+++ lua/ethan/plugins/treesitter.lua
@@ -1,10 +1,12 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
-        'nvim-treesitter/nvim-treesitter-context',
-        opts = { max_lines = 5 },
+        {
+            'nvim-treesitter/nvim-treesitter-context',
+            opts = { max_lines = 5 },
+        },
     },
     build = ':TSUpdate',
-    event = 'BufReadPost',
+    event = { 'BufReadPost', 'BufNewFile' },
     opts = require('ethan.plugins.opts.treesitter'),
 }
