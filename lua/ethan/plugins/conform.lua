8e3533e1af04c912109f0b20ce7f774d136d45f1
ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-03-07 20:27:54 +0200

    feat: switch from config to opts

--- lua/ethan/plugins/conform.lua
+++ lua/ethan/plugins/conform.lua
@@ -1,16 +1,17 @@
 return {
     'stevearc/conform.nvim',
     event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-        require('conform').setup({
-            formatters_by_ft = {
-                lua = { 'stylua' },
-                ['_'] = { 'trim_whitespace' },
-            },
-            format_on_save = {
-                timeout_ms = 500,
-                lsp_format = 'fallback',
-            },
-        })
-    end,
+    opts = {
+        formatters_by_ft = {
+            lua = { 'stylua' },
+            cpp = { 'clang-format' },
+            cmake = { 'cmake_format' },
+        },
+        format_on_save = {
+            timeout_ms = 3000,
+            async = false,
+            quiet = false,
+            lsp_format = 'fallback',
+        },
+    },
 }
