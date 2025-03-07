35fab4256af03e15138d78fe12f8ad92ea086daa
ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-03-07 20:27:29 +0200

    feat: move lualine config

--- lua/ethan/plugins/opts/lualine.lua
+++ lua/ethan/plugins/opts/lualine.lua
@@ -7,15 +7,7 @@ return {
         lualine_a = { 'mode' },
         lualine_b = { 'branch' },
         lualine_c = { 'filename' },
-        lualine_x = {
-            function()
-                if require('lazy.status').has_updates() then
-                    return '󰒲 󰚰 '
-                else
-                    return ''
-                end
-            end,
-        },
+        lualine_x = {},
         lualine_y = { 'filetype' },
         lualine_z = { 'location' },
     },
